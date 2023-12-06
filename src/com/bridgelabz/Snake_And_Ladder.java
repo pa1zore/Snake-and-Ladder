bf746eef90890ae69b6ac66611c04b8ca387e5d5
pa1zore <pavanzore345@gmail.com>
2023-12-06 21:09:23 +0530

    Modified: Count number of time dice roll and display player position

--- src/com/bridgelabz/Snake_And_Ladder.java
+++ src/com/bridgelabz/Snake_And_Ladder.java
@@ -1,18 +1,16 @@
 package com.bridgelabz;
 
 public class Snake_And_Ladder {
-    public static final int noPlay = 0; 
+    public static final int noPlay = 0;
     public static final int ladder = 1;
     public static final int snake = 2;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
         //Snake and Ladder game played with single player at start position 0
         int start = 0;
-
         int diceRoll = (int) (Math.random() * 6) + 1;
-
+        int c = 0;
         do{
-
             int options = (int)(Math.random()*3);
             switch(options){
                 case noPlay :
@@ -30,7 +28,9 @@ public class Snake_And_Ladder {
                     }
                     break;
             }
+            c++;
+            System.out.println("dice :"+diceRoll+" position: "+ start);
         }while(start<100);
-
+        System.out.println("Dice thrown: "+c);
     }
 }
