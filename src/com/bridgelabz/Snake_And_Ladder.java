a68975e99510274a2040f7fff6c4cc9c6e519611
pa1zore <pavanzore345@gmail.com>
2023-12-06 20:56:59 +0530

    Modified: Player rolls the die to get number 1 to 6

--- src/com/bridgelabz/Snake_And_Ladder.java
+++ src/com/bridgelabz/Snake_And_Ladder.java
@@ -5,5 +5,7 @@ public class Snake_And_Ladder {
         System.out.println("Welcome to Snake and Ladder game");
         //Snake and Ladder game played with single player at start position 0
         int start = 0;
+        
+        int diceRoll = (int) (Math.random() * 6) + 1;
     }
 }
