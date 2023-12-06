890cad0f5fa18db41244fd7105e02bd0638cd8c8
pa1zore <pavanzore345@gmail.com>
2023-12-06 20:59:44 +0530

    Modified: Check for option - Ladder, Snake or NoPlay

--- src/com/bridgelabz/Snake_And_Ladder.java
+++ src/com/bridgelabz/Snake_And_Ladder.java
@@ -1,11 +1,26 @@
 package com.bridgelabz;
 
 public class Snake_And_Ladder {
+    public static final int noPlay = 0;
+    public static final int ladder = 1;
+    public static final int snake = 2;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
         //Snake and Ladder game played with single player at start position 0
         int start = 0;
-        
+
         int diceRoll = (int) (Math.random() * 6) + 1;
+
+        int options = (int)(Math.random()*3);
+        switch(options){
+            case noPlay :
+                break;
+            case ladder:
+                start += diceRoll;
+                break;
+            case snake:
+                start -= diceRoll;
+                break;
+        }
     }
 }
